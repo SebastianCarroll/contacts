3d8ad90f6a05a77c4268f9d718eedfbedbe15e31
sebastian_carroll <sebastianjamescarroll@gmail.com>
2014-10-22 08:50:24 +1100

    Removed unecessary code and extracted setup method for clarity

--- integration/com/seb/storage/DatabaseIntegrationTest.java
+++ integration/com/seb/storage/DatabaseIntegrationTest.java
@@ -15,6 +15,10 @@ public class DatabaseIntegrationTest {
 
 	@Before
 	public void setup() throws IOException{
+		recreateDatabase();
+	}
+
+	private void recreateDatabase() throws IOException {
 		String path = "install";
 		ProcessBuilder pb = new ProcessBuilder("./install.sh");
 		pb.directory(new File(path));
@@ -31,39 +35,4 @@ public class DatabaseIntegrationTest {
 	public void test() {
 		//fail("Not yet implemented");
 	}
-	
-	private void runCommand(String command){
-		String s = null;
-		 
-        try {
-             
-        // run the Unix "ps -ef" command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec(command);
-             
-            BufferedReader stdInput = new BufferedReader(new
-                 InputStreamReader(p.getInputStream()));
- 
-            BufferedReader stdError = new BufferedReader(new
-                 InputStreamReader(p.getErrorStream()));
- 
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-             
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
-        }
-        catch (IOException e) {
-            System.out.println("exception happened - here's what I know: ");
-            e.printStackTrace();
-            System.exit(-1);
-        }
-	}
-
 }
