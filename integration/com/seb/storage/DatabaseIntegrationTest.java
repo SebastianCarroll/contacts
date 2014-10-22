cbcc66e3d9cfaf19f6113f825a4f1c18bf839b84
sebastian_carroll <sebastianjamescarroll@gmail.com>
2014-10-22 20:41:54 +1100

    Completed simple integrations/learning tests for the database

--- integration/com/seb/storage/DatabaseIntegrationTest.java
+++ integration/com/seb/storage/DatabaseIntegrationTest.java
@@ -1,20 +1,25 @@
 package com.seb.storage;
 
-import static org.junit.Assert.*;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DatabaseIntegrationTest {
 
 	@Before
-	public void setup() throws IOException{
+	public void setup() throws IOException {
 		recreateDatabase();
 	}
 
@@ -24,15 +29,51 @@ public class DatabaseIntegrationTest {
 		pb.directory(new File(path));
 		Process p = pb.start();
 		String s = null;
-		BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(p.getInputStream()));
-        while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-        }
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+				p.getInputStream()));
+		while ((s = stdInput.readLine()) != null) {
+			System.out.println(s);
+		}
 	}
-	
+
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		String url = "jdbc:mysql://localhost:3306/people";
+		String user = "testuser";
+		String password = "mypass";
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			st = con.createStatement();
+			rs = st.executeQuery("SELECT * from persons order by givenname limit 1");
+			System.out.println("Printing result set");
+			if (rs.next()) {
+				assertEquals("Acton", rs.getString(2));
+			} else {
+				Assert.fail("No results returned!");
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				System.out.println(ex);
+			}
+		}
 	}
 }
